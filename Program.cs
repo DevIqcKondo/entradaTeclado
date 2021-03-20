da4ce96c42b557ee2afab7e7d7f2550c5957ef7b
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-20 20:20:48 -0300

    Testando IF e ELSE

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace entradaTeclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Red;
+            float idade = 0;
+            Console.WriteLine("Qual é sua idade ?");
+		    idade = float.Parse(Console.ReadLine());
+            if(idade>=50){
+                Console.ForegroundColor=ConsoleColor.Yellow;
+              Console.WriteLine(" Está ficando velho! ");
+            }else if(idade<=1){
+                Console.ForegroundColor=ConsoleColor.Blue;
+             Console.WriteLine("Ainda é um bebê ");
+            } 
+            else if(idade==19){
+                Console.ForegroundColor=ConsoleColor.DarkMagenta;
+              Console.WriteLine("Eu também tenho "+idade+" anos!");
+            }
+             else{
+                 Console.ForegroundColor=ConsoleColor.Green;
+                 Console.WriteLine("Ainda está jovem!");
+             }
+             Console.ResetColor();
+        }
+    } 
+    } 
+
